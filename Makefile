4e59560b4b2f5ec2465d73ed82c97fea9571dbf5
Hugo Descamps <hdescamp@c2r8p4.42nice.fr>
2025-03-26 16:07:03 +0100

    And So It Begin

---
+++ Makefile
@@ -0,0 +1,64 @@
+.SILENT:
+
+#Flags
+
+CFLAGS=-Wall -Wextra -Werror -fsanitize=address -g
+
+#Files
+
+FILES= $(SRC_DIR)/philosophers.c \
+
+OBJS=$(FILES:.c=.o)
+
+#Directories
+
+SRC_DIR=src
+
+#Includes
+
+NAME=philo
+
+#Decoration
+
+CYAN = \033[1;36m
+GREEN = \033[1;32m
+RED = \031[1;31m
+RESET = \033[0m
+BOX_TOP = /===============\\
+BOX_MID = |
+BOX_MID_END = |
+BOX_BOT = \\===============/
+
+#Compilation
+
+$(NAME): $(OBJS)
+	@echo "$(RED)Making ??? ...$(RESET)"
+	@gcc $(CFLAGS) -o $(NAME) $(OBJS)
+	@echo "$(GREEN)??? Done Succesfully !$(RESET)"
+
+%.o: %.c
+	@gcc $(CFLAGS) -I includes -c $< -o $@
+
+#Commands
+
+all: $(NAME)
+	@echo "$(GREEN)All Done Succesfully !$(RESET)"
+	@echo "$(CYAN)$(BOX_TOP) $(RESET)"
+	@echo "$(CYAN)$(BOX_MID)$(RESET)$(GREEN)Executable Done$(RESET)$(CYAN)$(BOX_MID_END)$(RESET)"
+	@echo "$(CYAN)$(BOX_BOT)$(RESET)"
+
+clean:
+	@echo "$(RED)Cleaning object file in progress ...$(RESET)"
+	@rm -f $(OBJS)
+	@echo "$(GREEN)CLEAN Done Succesfully !$(RESET)"
+
+fclean: clean
+	@echo "$(RED)Full Clean in progress ...$(RESET)"
+	@rm -f $(NAME)
+	@echo "$(GREEN)FCLEAN Done Succesfully !$(RESET)"
+
+re: fclean all
+
+#phony
+
+.PHONY: all clean fclean re
---
+++ includes/philosophers.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdescamp <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 15:44:24 by hdescamp          #+#    #+#             */
+/*   Updated: 2025/03/26 15:44:27 by hdescamp         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+//includes
+
+# include <string.h>
+# include <limits.h>
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+//struct
+
+typedef struct s_vars
+{
+}		t_philo_data;
+
+#endif
